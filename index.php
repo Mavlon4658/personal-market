c77a0f33435af4758e002afe84fe06b1abcd1052
Mavlon4658 <mavlonbe4658>
2025-09-16 22:45:12 +0500

    rady|

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; url=https://personal-market.ru">
     <title>Document</title>
 
     <!-- Link Swiper's CSS -->
